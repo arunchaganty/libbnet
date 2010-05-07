5031bb952cbe149a8225855b937530853ea6d6d9
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2010-05-07 07:10:17 +0530

    Separated the NetShell

--- main.py
+++ main.py
@@ -1,58 +1,12 @@
 """
-Libbnet shell
+Libbnet 
 """
 
 from bnet.parsers import RaviParser, BNIFParser
 from bnet.BNet import *
+from shell import NetShell
 
 import sys
-import readline
-
-class NetShell():
-    """
-    Shell to interact with a Bayesian network
-    """
-
-    def __init__( self, net ):
-        self.net = net
-        self.running = False
-        self.context = []
-
-    def getContext( self ):
-        if self.context:
-            return self.context[-1]
-        else:
-            return None
-
-    def run( self ):
-        self.running = True
-        self.context = [ Context( self.net ) ]
-        while self.running:
-            try:
-                line = raw_input("[bnet]$ ")
-
-                if line.startswith('%'): self.shellFunc( line[1:] )
-
-            except EOFError:
-                break
-    
-    def shellFunc( self, cmd ):
-        args = cmd.split()
-        if args[0] == "vars":
-            if len( args ) > 1:
-                id = args[1]
-                print str( self.net.variables[ id ] )
-            else:
-                print self.net.variables.keys()
-        elif args[0] == "push":
-            self.context.append( Context( self.net, self.getContext() ) )
-        elif args[0] == "pop":
-            if len( self.context ) == 1:
-                print "Error: At base context"
-            else:
-                self.context.pop()
-        elif args[0] == "quit" or args[0] == "q":
-            self.running = False
 
 def main():
     if len( sys.argv ) != 2:
@@ -61,14 +15,16 @@ def main():
 
     filename = sys.argv[ 1 ]
 
-    p = BNIFParser()
-    n = p.parseFile( filename )
-
-    shell = NetShell( n )
-
-    shell.run()
-
-
+    n = None
+    for parser in [ RaviParser, BNIFParser ]:
+        try:
+            p = parser()
+            n = p.parseFile( filename )
+        except Exception:
+            continue
+    if n:
+        shell = NetShell( n )
+        shell.run()
 
 if __name__ == "__main__": main()
 
---
+++ shell.py
@@ -0,0 +1,67 @@
+"""
+Libbnet shell
+"""
+
+from bnet.parsers import RaviParser, BNIFParser
+from bnet.BNet import *
+
+import sys
+import readline
+
+class NetShell():
+    """
+    Shell to interact with a Bayesian network
+    """
+
+    def __init__( self, net ):
+        self.net = net
+        self.running = False
+        self.context = []
+
+    def getContext( self ):
+        if self.context:
+            return self.context[-1]
+        else:
+            return None
+
+    def run( self ):
+        self.running = True
+        self.context = [ Context( self.net ) ]
+        while self.running:
+            try:
+                line = raw_input("[bnet]$ ")
+
+                if line.startswith('%'): self.shellFunc( line[1:] )
+
+            except EOFError:
+                break
+
+    def shellFunc( self, cmd ):
+        args = cmd.split()
+        if args[0] == "vars":
+            if len( args ) > 1:
+                id = args[1]
+                print str( self.net.variables[ id ] )
+            else:
+                print self.net.variables.keys()
+        elif args[0] == "push":
+            self.context.append( Context( self.net, self.getContext() ) )
+        elif args[0] == "pop":
+            if len( self.context ) == 1:
+                print "Error: At base context"
+            else:
+                self.context.pop()
+        elif args[0] == "dot":
+            dot = self.net.toDot()
+            if len( args ) == 1:
+                print dot
+            elif len( args ) == 2:
+                fname = args[1]
+                f = open( fname, "w" )
+                f.write( dot )
+                f.close()
+            else:
+                print "Usage: %dot [<fname>]"
+        elif args[0] == "quit" or args[0] == "q":
+            self.running = False
+
